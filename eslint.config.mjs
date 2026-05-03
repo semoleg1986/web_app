b22151ba60ff4c02f6b0d3a36f7745d71d680eb0
semoleg1986 <semoleg1986@gmail.com>
2026-05-03 12:11:20 +0400

    chore(web): add oxlint fast lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,28 @@ import vue from "eslint-plugin-vue";
 import vueParser from "vue-eslint-parser";
 import eslintConfigPrettier from "eslint-config-prettier";
 
+const nuxtGlobals = {
+  computed: "readonly",
+  createError: "readonly",
+  defineEventHandler: "readonly",
+  defineNuxtConfig: "readonly",
+  getRouterParam: "readonly",
+  navigateTo: "readonly",
+  onMounted: "readonly",
+  process: "readonly",
+  reactive: "readonly",
+  ref: "readonly",
+  setHeader: "readonly",
+  useFetch: "readonly",
+  useHead: "readonly",
+  useRoute: "readonly",
+  useRuntimeConfig: "readonly",
+  useSeoMeta: "readonly",
+  useState: "readonly",
+  watch: "readonly",
+  watchEffect: "readonly"
+};
+
 export default [
   {
     ignores: [".nuxt/**", ".output/**", "node_modules/**", "coverage/**"]
@@ -20,7 +42,8 @@ export default [
         ecmaVersion: "latest",
         sourceType: "module",
         extraFileExtensions: [".vue"]
-      }
+      },
+      globals: nuxtGlobals
     },
     plugins: {
       "@typescript-eslint": tsEslint
@@ -33,6 +56,12 @@ export default [
           argsIgnorePattern: "^_"
         }
       ]
+    }
+  },
+  {
+    files: ["src/pages/**/*.vue"],
+    rules: {
+      "vue/multi-word-component-names": "off"
     }
   },
   eslintConfigPrettier
