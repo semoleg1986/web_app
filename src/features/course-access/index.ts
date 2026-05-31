e92dbbf30096e9b960dbba3b67710c4db9a4cdf6
semoleg1986 <semoleg1986@gmail.com>
2026-05-31 23:10:25 +0400

    chore(web_app): remove unused student access query

--- src/features/course-access/index.ts
+++
@@ -1,2 +0,0 @@
-export { useStudentCourseAccessQuery } from "~/features/course-access/api/use-student-course-access-query";
-export type { CourseAccessDecisionSnapshot } from "~/features/course-access/model/types";
